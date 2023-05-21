299a94a4e07f45d968597b9d61eeeedcc4a4f365
Nicolas GONFO <g_nicolas@live.fr>
2023-05-21 10:23:43 +0400

    Création d'un formulaire basic pour le moment pour la connexion admin et employés

---
+++ login_form.php
@@ -0,0 +1,7 @@
+<?php include 'header.php'; ?>
+<form action="login.php" method="POST">
+    <input type="email" name="email" placeholder="Adresse e-mail" required>
+    <input type="password" name="password" placeholder="Mot de passe" required>
+    <button type="submit">Se connecter</button>
+</form>
+
