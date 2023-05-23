3d1d72d11ef1161b04d2b5a70b4c00003406e107
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 12:23:42 +0400

    Page de connexion utilisateur les données envoyées sont récupéré et traité par 'login.php'

--- login_form.php
+++ login_form.php
@@ -1,7 +1,32 @@
-<?php include 'header.php'; ?>
-<form action="login.php" method="POST">
-    <input type="email" name="email" placeholder="Adresse e-mail" required>
-    <input type="password" name="password" placeholder="Mot de passe" required>
-    <button type="submit">Se connecter</button>
-</form>
+<?php
+include 'header.php';
+?>
 
+
+
+<h1>Page de connexion</h1>
+
+
+<!--FORMULAIRE DE CONNEXION-->
+<section class="section_conn_form">
+    <div class="conn_form">
+    <form action="login.php" method="POST">
+        <div class="form-group">
+            <label for="email">Adresse e-mail :</label>
+            <input type="email" name="email" id="email" placeholder="Adresse e-mail" required>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" placeholder="Mot de passe" required>
+        </div>
+
+        <button type="submit">Se connecter</button>
+    </form>
+    </div>
+</section>
+
+
+<?php
+include 'footer.php';
+?>
