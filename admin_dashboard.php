3c4fe0bd351bcdb59ffe712afb6e0d3a7a2c9cf4
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 13:06:03 +0400

    Page tableau de bord de l'admin. Contient liens vers le formulaire d'update des services, création de compte employé et modification des horaires d'ouverture

---
+++ admin_dashboard.php
@@ -0,0 +1,43 @@
+<?php
+// Vérifier si l'administrateur est authentifié
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (!isset($_SESSION['admin_token'])) {
+    // Rediriger vers la page de connexion si l'administrateur n'est pas authentifié
+    header("Location: login.php");
+    exit;
+}
+
+
+// Début du code HTML
+?>
+<?php include 'header.php' ?>
+
+
+    <h1>Tableau de bord administrateur</h1>
+
+
+    <!--FORMULAIRE SERVICES-->
+    <div>
+        <h3>Services</h3>
+        <a href="update_services.php">Modifier les services</a>
+    </div>
+
+
+    <!--FORMULAIRE COMPTE EMPLOYE-->
+    <div>
+        <h3>Compte employé</h3>
+        <a href="create_employee.php">Créer un compte</a>
+    </div>
+
+
+    <!--FORMULAIRE HORAIRES-->
+    <div>
+        <h3>Modifier les horaires d'ouverture</h3>
+        <a href="update_hours.php">Modifier les horaires</a>
+    </div>
+
+
+    <!--FOOTER-->
+    <?php include 'footer.php' ?>
