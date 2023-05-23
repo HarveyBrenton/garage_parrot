a992f62156c377982367570415f6e7153da027a0
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 12:28:15 +0400

    Page qui contient head+header du site qui sera inclus dans les pages accès public

--- header.php
+++ header.php
@@ -6,23 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Garage Parrot</title>
     <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@200&display=swap" rel="stylesheet">
+
 </head>
 <body>
-      <header>
+  <header>
     <div class="logo">
-      <img src="logo.png" alt="Garage V. Parrot Logo">
+      <img src="assets\img\logo-garage.png" alt="Garage V. Parrot Logo">
     </div>
     <nav>
       <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#vehicules">Véhicules d'occasion</a></li>
-        <li><a href="#temoignages">Témoignages</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="vehicles.php">Véhicules d'occasion</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
     <div class="login">
-    <a href="profile.php">Profile</a>
-      <a href="login_form.php">Se connecter</a>
+    <a href="profile.php">Mon compte</a>
+      <?php
+
+        if (isset($_SESSION['admin_token']) || isset($_SESSION['employee_token'])) {
+          // Utilisateur connecté, afficher le bouton "Se déconnecter"
+          echo '<a href="logout.php">Se déconnecter</a>';
+        } else {
+          // Utilisateur non connecté, afficher le bouton "Se connecter"
+          echo '<a href="login_form.php">Se connecter</a>';
+        }
+
+      ?>
     </div>
   </header>
