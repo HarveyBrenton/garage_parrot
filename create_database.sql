618fda33780fed20754bfc9058282589d32d47c2
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 12:50:53 +0400

    Fichier sql contenant les informations de la base de données et toutes les tables. J'ai pu 'SOURCE' avec mariaDB pour le traitement des données

--- create_database.sql
+++ create_database.sql
@@ -27,6 +27,8 @@ CREATE TABLE vehicles (
   year INT,
   mileage INT,
   image VARCHAR(255)
+  image1 VARCHAR(255)
+  image2 VARCHAR(255)
 );
 
 -- Table Reviews
@@ -36,4 +38,34 @@ CREATE TABLE Reviews (
   comment TEXT,
   reviewer_name VARCHAR(255),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  approved BOOLEAN DEFAULT 0
 );
+
+CREATE TABLE opening_hours (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  mon_hours VARCHAR(255) DEFAULT NULL,
+  tue_hours VARCHAR(255) DEFAULT NULL,
+  wed_hours VARCHAR(255) DEFAULT NULL,
+  thu_hours VARCHAR(255) DEFAULT NULL,
+  fri_hours VARCHAR(255) DEFAULT NULL,
+  sat_hours VARCHAR(255) DEFAULT NULL,
+  sun_hours VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE services (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  phone VARCHAR(20) NOT NULL,
+  message TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
