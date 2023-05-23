5fc27bf205ca76c1be7d9af9c8a5c12ca4db1d05
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 11:55:42 +0400

    Page de services qui détail mieux les services

---
+++ services.php
@@ -0,0 +1,55 @@
+<?php include 'cookies.php'; ?>
+<?php include 'header.php'; ?>
+
+  <!-- Section des services -->
+    <h1>Nos Services</h1>
+
+    <section>
+        <h2>Réparation carrosserie</h2>
+        <img src="assets\img\reparation-carosserie.jpeg" alt="Réparation carrosserie">
+        <p>La réparation carrosserie est 
+      l'une de nos spécialités. Que votre véhicule ait subi des 
+      dommages mineurs ou majeurs, notre équipe de professionnels 
+      expérimentés est là pour redonner à votre voiture son 
+      aspect d'origine. Nous utilisons des techniques avancées 
+      et des équipements de pointe pour réparer les rayures, les 
+      bosses et les déformations de la carrosserie, afin que 
+      votre véhicule retrouve son allure impeccable.</p>
+    </section>
+
+    <section>
+        <h2>Réparation mécanique</h2>
+        <img src="assets\img\reparation-mecanique.jpeg" alt="Réparation mécanique">
+        <p>Notre équipe qualifiée est 
+      spécialisée dans la réparation mécanique de tous types de 
+      véhicules. Que ce soit pour des problèmes de moteur, de 
+      transmission, de système de freinage ou d'autres composants 
+      essentiels, nous avons les compétences nécessaires pour 
+      diagnostiquer et résoudre efficacement les problèmes 
+      mécaniques. Nous utilisons des pièces de qualité et 
+      suivons des normes strictes pour assurer des réparations 
+      durables et fiables, vous permettant de reprendre la route 
+      en toute confiance.</p>
+    </section>
+
+    <section>
+        <h2>Entretien régulier</h2>
+        <img src="assets\img\entretien-regulier.jpeg" alt="Entretien régulier">
+        <p>L'entretien régulier de votre 
+        véhicule est essentiel pour assurer sa performance, 
+        sa sécurité et sa durabilité à long terme. Chez Garage 
+        Parrot, nous proposons une gamme complète de services 
+        d'entretien régulier, tels que la vidange d'huile, le 
+        remplacement des filtres, la vérification des niveaux 
+        de liquide, l'inspection des freins et des pneus, et 
+        bien plus encore. Nous veillons à ce que votre véhicule 
+        soit maintenu dans un état optimal, ce qui contribue à 
+        prévenir les pannes coûteuses et à prolonger sa durée de 
+        vie.</p>
+    </section>
+
+</body>
+
+
+
+</html>
