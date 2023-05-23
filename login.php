3112c0e7954a6c1bea6a6f9049cadbbfffedca2a
Nicolas GONFO <g_nicolas@live.fr>
2023-05-23 12:21:37 +0400

    Page de gestion de connexion : permet de vérifier l'administrateur ou employé

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-// Effectuer la connexion à la base de données (vous devrez configurer les informations de connexion appropriées)
+// Effectuer la connexion à la base de données
 $servername = "localhost";
 $username = "root";
 $password_db = "Harvey";
@@ -14,85 +14,53 @@ try {
     error_log($errorLog, 3, 'erreurs.log');
 
     // Redirection vers une page d'erreur personnalisée ou affichage d'un message d'erreur convivial
-    header('Location: server_error.html');
+    header('Location: server_error.php');
     exit;
 }
 
-    // Récupérer les informations soumises par le formulaire
-    $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    // Vérifier l'administrateur
-    $stmt = $conn->prepare("SELECT * FROM Admin WHERE email = :email LIMIT 1");
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+// Récupérer les valeurs du formulaire
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-    if ($result) {
-        if (password_verify($password, $result['password'])) {
-            // Authentification réussie en tant qu'administrateur
-            // Générer et stocker le jeton d'authentification
-            session_start();
-            $_SESSION['admin_token'] = bin2hex(random_bytes(32));
+// Vérifier l'administrateur
+$stmt = $conn->prepare("SELECT * FROM Admin WHERE email = :email LIMIT 1");
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->execute();
+$adminResult = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            // Effectuer les actions appropriées
-            // par exemple : header("Location: admin_dashboard.php");
-        } else {
-// Mot de passe incorrect
-// Gérer l'échec de l'authentification
-// par exemple : header("Location: login.php?error=1");
+// Vérifier si l'administrateur est authentifié
+if ($adminResult && password_verify($password, $adminResult['password'])) {
+    // Authentification réussie en tant qu'administrateur
+    // Générer et stocker le jeton d'authentification
+    session_start();
+    $_SESSION['admin_token'] = bin2hex(random_bytes(32));
+
+    // redirection dashboard admin
+    header("Location: admin_dashboard.php");
+    exit;
 }
+
+// Vérifier l'employé
+$stmt = $conn->prepare("SELECT * FROM Employees WHERE email = :email LIMIT 1");
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->execute();
+$employeeResult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Vérifier si l'employé est authentifié
+if ($employeeResult && password_verify($password, $employeeResult['password'])) {
+    // Authentification réussie en tant qu'employé
+    // Générer et stocker le jeton d'authentification
+    session_start();
+    $_SESSION['employee_token'] = bin2hex(random_bytes(32));
+
+    // redirection dashboard
+    header("Location: employee_dashboard.php");
+    exit;
 } else {
-    // Vérifier la table "Employees" pour les correspondances
-    $stmt = $conn->prepare("SELECT * FROM Employees WHERE email = :email LIMIT 1");
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        if (password_verify($password, $result['password'])) {
-            // Authentification réussie en tant qu'employé
-            // Générer et stocker le jeton d'authentification
-            session_start();
-            $_SESSION['employee_token'] = bin2hex(random_bytes(32));
-
-            // Effectuer les actions appropriées
-            // par exemple : header("Location: employee_dashboard.php");
-        } else {
-            // Mot de passe incorrect
-            // Gérer l'échec de l'authentification
-            // par exemple : header("Location: login.php?error=1");
-        }
-    } else {
-        // Aucune correspondance trouvée dans les tables "Admin" et "Employees"
-        // Gérer l'échec de l'authentification
-        // par exemple : header("Location: login.php?error=1");
-    }
+    // Si l'authentification a échoué : redirection login
+    header("Location: login.php?error=1");
+    exit;
 }
-
-// Insertion d'un enregistrement d'administrateur
-try {
-    $conn->beginTransaction();
-
-    $stmt = $conn->prepare("INSERT INTO Admin (email, password) VALUES (:email, :password)");
-    $email_admin = "admin@example.com";
-    $password_admin = password_hash("admin123", PASSWORD_DEFAULT);
-    $stmt->bindParam(':email', $email_admin);
-    $stmt->bindParam(':password', $password_admin);
-    $stmt->execute();
-
-    $conn->commit();
-} catch (PDOException $e) {
-    $conn->rollBack();
-    $errorLog = 'Erreur lors de la connexion à la base de données' . $e->getMessage() . PHP_EOL;
-    error_log($errorLog, 3, 'erreurs.log');
-}
-
-// Fermer la connexion à la base de données
-$stmt = null;
-$conn = null;
-
-?>
-
 
 ?>
